e89458c14c74f4f76c900ad0915b1b1fe4bcaf60
Hieu Ngo <hieua0609ptnk@hotmail.com>
2015-11-19 01:05:31 -0700

    doing dashboard

--- client/templates/dashboard.js
+++ client/templates/dashboard.js
@@ -1,3 +1,39 @@
 /**
  * Created by Hieu on 11/15/2015.
  */
+Template.dashboard.onCreated(function(){
+    this.apartmentSelected = new ReactiveVar(undefined);
+    this.buildingSelected = new ReactiveVar(undefined);
+});
+Template.dashboard.helpers({
+    apartments: function(){
+        return Apartment.find({});
+    },
+    buildings: function(){
+        var ap = Template.instance().apartmentSelected.get();
+        return (ap)? ap.buildings() : [];
+    },
+    homes: function(){
+        var bu = Template.instance().buildingSelected.get();
+        return bu.homes().map(function(doc, index, cursor){
+            var i = _.extend(doc,{index: index});
+            return i;
+        });
+    },
+    isRemainder: function(index, rem){
+        return index % 3 == rem;
+    }
+});
+
+Template.dashboard.events({
+    'change #apartment-selection-dashboard': function(event){
+        var target = event.currentTarget;
+        var newId = target.options[target.selectedIndex].value;
+        Template.instance().apartmentSelected.set(Apartment.findOne({_id: newId}));
+    },
+    'click #building-list .list-group-item': function(event){
+        var target = event.currentTarget;
+        var newId = target.value;
+        Template.instance().buildingSelected.set(Building.findOne({_id: newId}));
+    }
+});
