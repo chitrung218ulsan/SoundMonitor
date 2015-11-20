aec35038eab260333eddfd77ac9a9f15d07736db
Hieu Ngo <hieua0609ptnk@hotmail.com>
2015-11-19 19:02:02 -0700

    dashboard

--- client/templates/dashboard.js
+++ client/templates/dashboard.js
@@ -15,10 +15,10 @@ Template.dashboard.helpers({
     },
     homes: function(){
         var bu = Template.instance().buildingSelected.get();
-        return bu.homes().map(function(doc, index, cursor){
+        return (bu)? bu.homes().map(function(doc, index, cursor){
             var i = _.extend(doc,{index: index});
             return i;
-        });
+        }) : [];
     },
     isRemainder: function(index, rem){
         return index % 3 == rem;
@@ -30,6 +30,7 @@ Template.dashboard.events({
         var target = event.currentTarget;
         var newId = target.options[target.selectedIndex].value;
         Template.instance().apartmentSelected.set(Apartment.findOne({_id: newId}));
+        Template.instance().buildingSelected.set(undefined);
     },
     'click #building-list .list-group-item': function(event){
         var target = event.currentTarget;
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,37 +1,101 @@
 Meteor.startup(function()
 {
-	
-	//Apartment.remove({});
-	//Building.remove({});
-	//var apart1 =
-	//{
-	//	name: "Lotte",
-	//	address: "Ulsan",
-	//	constructDate: new Date(),
-	//	manager: "Trung",
-	//	createdBy: "Trung",
-	//	remarks: "Lotte"
-	//};
-	////var apartId = findAparment('Lotte');
-	////console.log(apartId);
-	//var building1 = {
-	//	name: "Building 1",
-	//	buildingNumber: 1,
-	//	numOfFloors: 7,
-	//	numHousePerFloor: 6,
-	//	type: "complex",
-	//	createdBy: "Trung",
-	//	remarks: "Building 1"
-	//};
-	//
-	//Apartment.insert(apart1, function(err,obj){
-	//	if(err){
-	//		console.log(err);
-	//		return;
-	//	}
-	//	building1.apartmentId = obj;
-	//	Building.insert(building1);
-	//});
+
+	var apart1 =
+	{
+		name: "Lotte",
+		address: "Ulsan",
+		constructDate: new Date(),
+		manager: "Trung",
+		createdBy: "Trung",
+		remarks: "Lotte"
+	};
+	//var apartId = findAparment('Lotte');
+	//console.log(apartId);
+	var building1 = {
+		name: "Building 1",
+		buildingNumber: 1,
+		numOfFloors: 7,
+		numHousePerFloor: 6,
+		type: "complex",
+		createdBy: "Trung",
+		remarks: "Building 1"
+	};
+
+	var building2 = {
+		name: "Building 2",
+		buildingNumber: 3,
+		numOfFloors: 2,
+		numHousePerFloor: 3,
+		type: "simple",
+		createdBy: "Hieu Ngo",
+		remarks: "Building 2"
+	};
+
+	var home1 = {
+		floor: 1,
+		homeNumber: 101,
+		owner: 'Hieu',
+		telNumber: '7889302',
+		createdBy: "Hieu Ngo",
+		nodeId: 'unknown'
+	};
+
+	var home2 = {
+		floor: 2,
+		homeNumber: 201,
+		owner: 'Hieu',
+		telNumber: '9999999',
+		createdBy: "Hieu Ngo",
+		nodeId: 'unknown'
+	};
+
+	var home3 = {
+		floor: 1,
+		homeNumber: 301,
+		owner: 'Hieu',
+		telNumber: '7889302',
+		createdBy: "Hieu Ngo",
+		nodeId: 'unknown'
+	};
+
+	var home4 = {
+		floor: 2,
+		homeNumber: 401,
+		owner: 'Hieu',
+		telNumber: '9999999',
+		createdBy: "Hieu Ngo",
+		nodeId: 'unknown'
+	};
+
+	Apartment.insert(apart1, function(err,obj){
+		if(err){
+			console.log(err);
+			return;
+		}
+		building1.apartmentId = obj;
+		building2.apartmentId = obj;
+		Building.insert(building1,function(err,bu1){
+			home1.buildingId = bu1;
+			home3.buildingId = bu1;
+			home4.buildingId = bu1;
+			Home.insert(home1, function(err,hu1){
+				console.log(err);
+			});
+			Home.insert(home3);
+			Home.insert(home4);
+		});
+		Building.insert(building2, function(err,bu2){
+			home2.buildingId = bu2;
+			home3.buildingId = bu2;
+			home4.buildingId = bu2;
+			Home.insert(home2,function(err,hu2){
+				console.log(err);
+			});
+			Home.insert(home3);
+			Home.insert(home4);
+		});
+	});
 
 	
 	
